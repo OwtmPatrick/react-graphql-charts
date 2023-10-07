68d4898701fb6c9dd3a631ef34d45a9abc9a18ae
Radik Zangirov <owtmpatrick@gmail.com>
2023-10-07 22:36:24 +0500

    gh pages path

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,17 +4,20 @@ import { Routes } from '../constants/routes';
 import { Login } from '../pages/Login';
 import { Dashboard } from '../pages/Dashboard';
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Navigate to={Routes.DASHBOARD} replace />
-  },
-  {
-    path: Routes.LOGIN,
-    element: <Login />
-  },
-  {
-    path: Routes.DASHBOARD,
-    element: <Dashboard />
-  }
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Navigate to={Routes.DASHBOARD} replace />
+    },
+    {
+      path: Routes.LOGIN,
+      element: <Login />
+    },
+    {
+      path: Routes.DASHBOARD,
+      element: <Dashboard />
+    }
+  ],
+  { basename: '/react-graphql-charts' }
+);
