fb74fb9305a7403da8cbcc9cf02dc27ba2b70df7
Radik Zangirov <owtmpatrick@gmail.com>
2023-10-09 14:35:45 +0500

    remove redundant code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-// import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
-// import './App.css';
 import 'antd/dist/reset.css';
 
 import { RouterProvider } from 'react-router-dom';
